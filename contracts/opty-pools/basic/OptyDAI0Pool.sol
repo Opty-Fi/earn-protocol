c51e0597c8f135b739dd7db4162035c9700942e7
Deepanshu Gupta <312.gupta@gmail.com>
2020-10-05 19:53:09 -0400

    - Added invest() for depositing DAI into contract and in return giving opDai tokens - Added interfaces, libraries - Added utils folders and addeded Context, ERC20, ERC20Detailed, Modifiers contract

--- contracts/opty-pools/basic/OptyDAI0Pool.sol
+++ contracts/opty-pools/basic/OptyDAI0Pool.sol
@@ -2,6 +2,32 @@
 
 pragma solidity ^0.6.10;
 
-contract OptyDAI0Pool{
+import "./../../libraries/SafeMath.sol";
+import "./../../libraries/Addresses.sol";
+import "./../../libraries/SafeERC20.sol";
+import "./../../utils/Context.sol";
+import "./../../utils/ERC20.sol";
+import "./../../utils/ERC20Detailed.sol";
+import "./../../utils/Modifiers.sol";
+
+contract OptyDAI0Pool is ERC20, ERC20Detailed, Modifiers {
+    using SafeERC20 for IERC20;
     
+    address public token;
+    address public compound;
+    
+    constructor () public ERC20Detailed("Opty Fi DAI", "opDai", 18) {
+        token = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);    //  DAI token contract address
+        
+    }
+    
+    function invest(uint256 _amount) external nonReentrant returns(bool _success) {
+        require(_amount > 0, "deposit must be greater than 0");
+        
+        IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
+        
+        _mint(msg.sender, _amount); 
+        _success = true;
+    }
+
 }
---
+++ contracts/utils/Modifiers.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.10;
+
+/**
+ * @dev Contract used to keep all the modifiers at one place
+ */
+contract Modifiers {
+    
+    address public owner;
+    address public governance;
+    address public strategist;
+    uint256 private _guardCounter;
+    
+    /**
+     * @dev Sets the owner, governance and strategist while deploying the contract
+     */
+    constructor () internal {
+        owner = msg.sender;
+        governance = msg.sender;
+        strategist = msg.sender;
+        _guardCounter = 1;
+    }
+    
+    /**
+     * @dev Modifier to check if the address is zero address or not
+     */
+    modifier onlyValidAddress(){
+        require(msg.sender != address(0), "caller is zero address");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check caller is owner or not
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "caller is not owner");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check caller is governance or not
+     */
+    modifier onlyGovernance() {
+        require(msg.sender == governance, "caller is not having governance");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check caller is strategist or not
+     */
+    modifier onlyStrategist() {
+        require(msg.sender == strategist, "Caller is not strategist");
+        _;
+    }
+
+    modifier nonReentrant() {
+        _guardCounter += 1;
+        uint256 localCounter = _guardCounter;
+        _;
+        require(localCounter == _guardCounter, "ReentrancyGuard: reentrant call");
+    }
+}
