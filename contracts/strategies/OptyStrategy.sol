fb5698b55f4c4ec93cd0fab1d081ece140619353
dhruvin <dhruvin.parikh@georgebrown.ca>
2020-10-22 14:25:08 -0400

    valid address,amount, strategy length check

--- contracts/strategies/OptyStrategy.sol
+++ contracts/strategies/OptyStrategy.sol
@@ -19,7 +19,7 @@ contract OptyStrategy {
 
 
     constructor(address _optyRegistry) public {
-          governance = msg.sender;
+        governance = msg.sender;
         optyRegistry = _optyRegistry;
     }
 
@@ -32,6 +32,7 @@ contract OptyStrategy {
         _balance = 0;
         if(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000) {
          IOptyRegistry.StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
+        require(_strategySteps.length > 0,"!_strategySteps.length");
         if(_strategySteps.length == 1){
             _balance = singleStepBalance(_strategySteps, _account);
         }
@@ -68,7 +69,9 @@ contract OptyStrategy {
 
     }
     
-    function deploy(uint _amount, bytes32 _hash) public returns(bool _success) {
+    function deploy(uint _amount, bytes32 _hash) public onlyValidAddress returns(bool _success) {
+    require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash");   
+    require(_amount > 0, "!_amount");
     IOptyRegistry.StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
         if(_strategySteps.length == 1) {
             require(singleStepDeploy(_amount,_strategySteps),"!singleStepDeploy()");
@@ -79,7 +82,9 @@ contract OptyStrategy {
         _success = true;
     }
     
-    function singleStepDeploy(uint _amount, IOptyRegistry.StrategyStep[] memory _strategySteps) public returns(bool _success) {
+    function singleStepDeploy(uint _amount, IOptyRegistry.StrategyStep[] memory _strategySteps) public onlyValidAddress returns(bool _success) {
+        require(_amount > 0, "!_amount");
+        require(_strategySteps.length == 1,"!_strategySteps.length");
         IERC20(_strategySteps[0].token).safeTransfer(_strategySteps[0].poolProxy, _amount);
         require(IOptyLiquidityPoolProxy(_strategySteps[0].poolProxy).
         deploy(_strategySteps[0].token, _strategySteps[0].liquidityPool,_strategySteps[0].lendingPoolToken, _amount));
@@ -90,7 +95,9 @@ contract OptyStrategy {
         _success = true;
     }
     
-    function recall(uint _amount, bytes32 _hash) public returns(bool _success) {
+    function recall(uint _amount, bytes32 _hash) public onlyValidAddress returns(bool _success) {
+        require(_hash != 0x0000000000000000000000000000000000000000000000000000000000000000,"!_hash"); 
+        require(_amount > 0, "!_amount");
         IOptyRegistry.StrategyStep[] memory _strategySteps = _getStrategySteps(_hash);
         if(_strategySteps.length == 1) {
             require(singleStepRecall(_amount,_strategySteps),"!singleStepDeploy()");
@@ -101,7 +108,9 @@ contract OptyStrategy {
         _success = true;
     }
     
-    function singleStepRecall(uint _amount, IOptyRegistry.StrategyStep[] memory _strategySteps) public returns(bool _success) {
+    function singleStepRecall(uint _amount, IOptyRegistry.StrategyStep[] memory _strategySteps) public onlyValidAddress returns(bool _success) {
+        require(_amount > 0, "!_amount");
+        require(_strategySteps.length == 1,"!_strategySteps.length");
         IERC20(_strategySteps[0].lendingPoolToken).safeTransfer(_strategySteps[0].poolProxy,_amount);
         require(IOptyLiquidityPoolProxy(_strategySteps[0].poolProxy).recall(_strategySteps[0].token,_strategySteps[0].lendingPoolToken,_amount));
         IERC20(_strategySteps[0].token).safeTransfer(msg.sender,IERC20(_strategySteps[0].token).balanceOf(address(this)));
@@ -131,6 +140,14 @@ contract OptyStrategy {
      */
     modifier onlyGovernance() {
         require(msg.sender == governance, "!governance");
+        _;
+    }
+    
+    /**
+     * @dev Modifier to check if the address is zero address or not
+     */
+    modifier onlyValidAddress(){
+        require(msg.sender != address(0), "zero address");
         _;
     }
 }
