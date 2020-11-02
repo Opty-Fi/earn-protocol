ee59ce5623195783c6360304a334d6a6cacc3827
dhruvinparikh <imsilent93@gmail.com>
2020-11-02 12:48:40 -0500

    remove redundant strategies

--- contracts/interfaces/opty/IOptyRegistry.sol
+++ contracts/interfaces/opty/IOptyRegistry.sol
@@ -32,7 +32,6 @@ interface IOptyRegistry{
 
     function getTokenToStrategies(bytes32 _tokensHash) external view returns(bytes32[] memory);
     function getStrategy(bytes32 _hash) external view returns(uint8 _score, bool _isStrategy, uint256 _index, uint256 _blockNumber, StrategyStep[] memory _strategySteps);
-    function getTokenStrategies(bytes32 _tokensHash) external view returns(bytes32[] memory);
     function liquidityPools(address _pool) external view returns(LiquidityPool memory);
     function getLiquidityPoolToken(bytes32 _hash) external view returns(address _lendingPool);
     function getTokensHashToTokens(bytes32 _tokensHash) external view returns(address[] memory);
