46ee03edb308471e0f952c98619a144bec10e085
dhruvinparikh <imsilent93@gmail.com>
2021-01-13 20:01:03 -0500

    apply linter to aave interface

--- contracts/interfaces/aave/IAaveCore.sol
+++ contracts/interfaces/aave/IAaveCore.sol
@@ -4,8 +4,27 @@ pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
 interface IAaveCore {
-    function getReserves() external view returns(address[] memory);
-    function getUserBasicReserveData(address _reserve, address _user) external view returns (uint256, uint256, uint256, bool);
-    function getReserveConfiguration(address _reserve) external view returns (uint256, uint256, uint256, bool);   
-    function isUserUseReserveAsCollateralEnabled(address _reserve, address _user) external view returns(bool);
+    function getReserves() external view returns (address[] memory);
+
+    function getUserBasicReserveData(address _reserve, address _user)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            bool
+        );
+
+    function getReserveConfiguration(address _reserve)
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            bool
+        );
+
+    function isUserUseReserveAsCollateralEnabled(address _reserve, address _user) external view returns (bool);
 }
--- contracts/interfaces/aave/IAaveDataProvider.sol
+++ contracts/interfaces/aave/IAaveDataProvider.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
 interface IAaveDataProvider {
-function calculateUserGlobalData(address _user) external view returns (
-    uint256 totalLiquidityBalanceETH,
-    uint256 totalCollateralBalanceETH, 
-    uint256 totalBorrowBalanceETH, 
-    uint256 totalFeesETH, 
-    uint256 currentLtv, 
-    uint256 currentLiquidationThreshold, 
-    uint256 healthFactor, 
-    bool healthFactorBelowThreshold);
+    function calculateUserGlobalData(address _user)
+        external
+        view
+        returns (
+            uint256 totalLiquidityBalanceETH,
+            uint256 totalCollateralBalanceETH,
+            uint256 totalBorrowBalanceETH,
+            uint256 totalFeesETH,
+            uint256 currentLtv,
+            uint256 currentLiquidationThreshold,
+            uint256 healthFactor,
+            bool healthFactorBelowThreshold
+        );
 }
