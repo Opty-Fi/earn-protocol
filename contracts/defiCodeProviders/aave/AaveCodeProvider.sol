db2cf6082326e894ab921e4cacff45d4c06da270
dhruvinparikh <imsilent93@gmail.com>
2021-01-15 13:47:22 -0500

    add borrow repay code provider

--- contracts/defiCodeProviders/aave/AaveCodeProvider.sol
+++ contracts/defiCodeProviders/aave/AaveCodeProvider.sol
@@ -4,17 +4,23 @@ pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
 import "../../interfaces/opty/ICodeProvider.sol";
+import "../../interfaces/aave/IPriceOracle.sol";
 import "../../interfaces/aave/ILendingPoolAddressesProvider.sol";
 import "../../interfaces/aave/IAave.sol";
 import "../../interfaces/aave/IAToken.sol";
 import "../../interfaces/ERC20/IERC20.sol";
 import "../../libraries/SafeMath.sol";
 import "../../utils/Modifiers.sol";
+import "../../utils/ERC20Detailed.sol";
 
 contract AaveCodeProvider is ICodeProvider, Modifiers {
     using SafeMath for uint256;
 
     uint256 public maxExposure; // basis points
+    
+    uint256 public healthFactor = 4;
+    uint256 public ltv = 65;
+    uint256 public max = 100;
 
     constructor(address _registry) public Modifiers(_registry) {
         setMaxExposure(uint256(5000)); // 50%
@@ -50,6 +56,37 @@ contract AaveCodeProvider is ICodeProvider, Modifiers {
         uint256[] memory _amounts = new uint256[](1);
         _amounts[0] = IERC20(_underlyingTokens[0]).balanceOf(_optyPool);
         return getDepositSomeCodes(_optyPool, _underlyingTokens, _liquidityPoolAddressProvider, _amounts);
+    }
+    
+    function getBorrowAllCodes(address _optyPool, address _underlyingToken, address _liquidityPoolAddressProvider, address _outputToken) public view returns(bytes[] memory _codes) {
+        address _lendingPool = _getLendingPool(_liquidityPoolAddressProvider);
+        ReserveConfigurationData memory _reserveConfigurationData = IAave(_lendingPool).getReserveConfigurationData(_underlyingToken);
+        if(_reserveConfigurationData.usageAsCollateralEnabled && _reserveConfigurationData.stableBorrowRateEnabled) {
+        uint256 _borrow = _availableToBorrowReserve(_optyPool, _liquidityPoolAddressProvider, _outputToken);
+        if (_borrow > 0) {
+            bool _isUserCollateralEnabled = IAave(_lendingPool).getUserReserveData(_underlyingToken,_optyPool).enabled;
+            if(_isUserCollateralEnabled) {
+                _codes = new bytes[](1);
+                _codes[0] = abi.encode(_lendingPool,abi.encodeWithSignature("borrow(address, uint256, uint256, uint16)", _outputToken, _borrow, 1, 0));   
+            } else {
+                _codes = new bytes[](2);
+                _codes[0] = abi.encode(_lendingPool,abi.encodeWithSignature("setUserUseReserveAsCollateral(address, bool)",_underlyingToken, true));
+                _codes[1] = abi.encode(_lendingPool,abi.encodeWithSignature("borrow(address, uint256, uint256, uint16)", _outputToken, _borrow, 1, 0));
+            }
+        }
+        } else {
+            revert("!borrow");
+        }
+    }
+    
+    function getRepayAllCodes(address _optyPool, address _liquidityPoolAddressProvider, address _outputToken) public view returns(bytes[] memory _codes) {
+        address _lendingPoolCore = _getLendingPoolCore(_liquidityPoolAddressProvider);
+        address _lendingPool = _getLendingPool(_liquidityPoolAddressProvider);
+        uint _amount = IERC20(_outputToken).balanceOf(_optyPool);
+        _codes = new bytes[](3);
+        _codes[0] = abi.encode(_outputToken, abi.encodeWithSignature("approve(address,uint256)", _lendingPoolCore, uint256(0)));
+        _codes[1] = abi.encode(_outputToken, abi.encodeWithSignature("approve(address,uint256)", _lendingPoolCore, _amount));
+        _codes[2] = abi.encode(_lendingPool, abi.encodeWithSignature("repay(address, address, address)",_outputToken, _amount, address(uint160(_optyPool))));
     }
 
     function getWithdrawSomeCodes(
@@ -247,6 +284,10 @@ contract AaveCodeProvider is ICodeProvider, Modifiers {
     function _getLendingPoolCore(address _lendingPoolAddressProvider) internal view returns (address) {
         return ILendingPoolAddressesProvider(_lendingPoolAddressProvider).getLendingPoolCore();
     }
+    
+    function _getPriceOracle(address _lendingPoolAddressProvider) internal view returns (address) {
+        return ILendingPoolAddressesProvider(_lendingPoolAddressProvider).getPriceOracle();
+    }
 
     function _getDepositAmount(
         address _liquidityPoolAddressProvider,
@@ -258,6 +299,70 @@ contract AaveCodeProvider is ICodeProvider, Modifiers {
         uint256 _limit = (_poolValue.mul(maxExposure)).div(uint256(10000));
         if (_depositAmount > _limit) {
             _depositAmount = _limit;
+        }
+    }
+    
+    function _maxSafeETH(address _optyPool, address _liquidityPoolAddressProvider) public view returns (uint256 maxBorrowsETH, uint256 totalBorrowsETH, uint256 availableBorrowsETH) {
+        UserAccountData memory _userAccountData = IAave(_getLendingPool(_liquidityPoolAddressProvider)).getUserAccountData(_optyPool);
+        uint256 _totalBorrowsETH = _userAccountData.totalBorrowsETH;
+        uint256 _availableBorrowsETH = _userAccountData.availableBorrowsETH;
+        uint256 _maxBorrowETH = (_totalBorrowsETH.add(_availableBorrowsETH));
+        return (_maxBorrowETH.div(healthFactor), _totalBorrowsETH, _availableBorrowsETH);
+    }
+    
+    
+    function _availableToBorrowETH(address _optyPool, address _liquidityPoolAddressProvider) public view returns (uint256) {
+        (uint256 _maxSafeETH_, uint256 _totalBorrowsETH, uint256 _availableBorrowsETH) = _maxSafeETH(_optyPool, _liquidityPoolAddressProvider);
+        _maxSafeETH_ = _maxSafeETH_.mul(95).div(100); // 5% buffer so we don't go into a earn/rebalance loop
+        if (_maxSafeETH_ > _totalBorrowsETH) {
+            return _availableBorrowsETH.mul(_maxSafeETH_.sub(_totalBorrowsETH)).div(_availableBorrowsETH);
+        } else {
+            return 0;
+        }
+    }
+    
+    function _getReservePrice(address _liquidityPoolAddressProvider, address _token) public view returns (uint256) {
+        return _getReservePriceETH(_liquidityPoolAddressProvider, _token);
+    }
+    
+    function _getReservePriceETH(address _liquidityPoolAddressProvider, address _token) public view returns (uint256) {
+        return IPriceOracle(_getPriceOracle(_liquidityPoolAddressProvider)).getAssetPrice(_token);
+    }
+    
+    function _availableToBorrowReserve(address _optyPool, address _liquidityPoolAddressProvider, address _outputToken) public view returns (uint256) {
+        uint256 _available = _availableToBorrowETH(_optyPool, _liquidityPoolAddressProvider);
+        if (_available > 0) {
+            return _available.mul(uint256(10)**ERC20Detailed(_outputToken).decimals()).div(_getReservePrice(_liquidityPoolAddressProvider, _outputToken));
+        } else {
+            return 0;
+        }
+    }
+    
+    function _getUnderlyingPrice(address _liquidityPoolAddressProvider, address _underlyingToken) public view returns (uint256) {
+        return _getReservePriceETH(_liquidityPoolAddressProvider, _underlyingToken);
+    }
+    
+    function _getUnderlyingPriceETH(address _underlyingToken, address _liquidityPoolAddressProvider, uint256 _amount) public view returns (uint256) {
+        address _liquidityPoolToken = getLiquidityPoolToken(_underlyingToken, _liquidityPoolAddressProvider);
+        _amount = _amount.mul(_getUnderlyingPrice(_liquidityPoolAddressProvider,_underlyingToken)).div(uint256(10)**ERC20Detailed(address(_liquidityPoolToken)).decimals()); // Calculate the amount we are withdrawing in ETH
+        return _amount.mul(ltv).div(max).div(healthFactor);
+    }
+    
+    function _over(address _optyPool, address _underlyingToken, address _liquidityPoolAddressProvider, address _outputToken, uint256 _amount) public view returns (uint256) {
+        uint256 _eth = _getUnderlyingPriceETH(_underlyingToken, _liquidityPoolAddressProvider, _amount);
+        (uint256 _maxSafeETH_, uint256 _totalBorrowsETH, ) = _maxSafeETH(_optyPool, _liquidityPoolAddressProvider);
+        _maxSafeETH_ = _maxSafeETH_.mul(105).div(100); // 5% buffer so we don't go into a earn/rebalance loop
+        if (_eth > _maxSafeETH_) {
+            _maxSafeETH_ = 0;
+        } else {
+            _maxSafeETH_ = _maxSafeETH_.sub(_eth); // Add the ETH we are withdrawing
+        }
+        if (_maxSafeETH_ < _totalBorrowsETH) {
+            uint256 _over_ = _totalBorrowsETH.mul(_totalBorrowsETH.sub(_maxSafeETH_)).div(_totalBorrowsETH);
+            _over_ = _over_.mul(uint256(10)**ERC20Detailed(_outputToken).decimals()).div(_getReservePrice(_liquidityPoolAddressProvider, _outputToken));
+            return _over_;
+        } else {
+            return 0;
         }
     }
 }
