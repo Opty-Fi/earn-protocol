c4b2fc1399e2c4126de7434f7336ce558bd60702
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2021-03-02 15:34:21 +0100

    Staking contracts created

---
+++ contracts/OPTYToken/StakingPoolStorage.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.10;
+
+import "./../RiskManager.sol";
+import "./../StrategyCodeProvider.sol";
+import "./../OPTYToken/OPTYMinter.sol";
+
+contract StakingPoolStorage {
+    struct StakingState {
+        uint32 timestamp;
+        uint224 value;
+    }
+    mapping (address => StakingState) userState;
+    uint32 lastPoolUpdate;
+    uint256 poolIndex;
+    uint256 optyRatePerBlock;
+    address public token; //  store the underlying token contract address (for example DAI)
+    uint256 public poolValue;
+    OPTYMinter public optyMinterContract;
+}
