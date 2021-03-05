4f4a5e46db0761b69ed10c090bb01b2f30bf8f20
dhruvinparikh <imsilent93@gmail.com>
2021-03-05 18:25:19 -0500

    merge from dev

--- contracts/OPTYToken/OPTYMinter.sol
+++ contracts/OPTYToken/OPTYMinter.sol
@@ -8,7 +8,19 @@ import "./ExponentialNoError.sol";
 import "./../interfaces/ERC20/IERC20.sol";
 
 contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
-    constructor(address _registry) public Modifiers(_registry) {}
+    constructor(address _registry, address _stakingPool) public Modifiers(_registry) {
+        setStakingPool(_stakingPool);
+    }
+
+    function setStakingPool(address _stakingPool) public onlyOperator {
+        require(_stakingPool != address(0), "Invalid address");
+        optyStakingPool = OPTYStakingPool(_stakingPool);
+    }
+
+    function claimAndStake(address holder) public {
+        uint256 _amount = claimOpty(holder, allOptyPools);
+        optyStakingPool.userStake(_amount);
+    }
 
     /**
      * @notice Claim all the opty accrued by holder in all markets
@@ -34,7 +46,8 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
      * @param holders The addresses to claim OPTY for
      * @param optyTokens The list of markets to claim OPTY in
      */
-    function claimOpty(address[] memory holders, address[] memory optyTokens) public {
+    function claimOpty(address[] memory holders, address[] memory optyTokens) public returns (uint256) {
+        uint256 _total;
         for (uint256 i = 0; i < optyTokens.length; i++) {
             address _optyToken = optyTokens[i];
             require(optyPoolEnabled[_optyToken], "optyPool must be enabled");
@@ -67,14 +80,14 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
         for (uint256 i = 0; i < optyTokens.length; i++) {
             address _optyToken = optyTokens[i];
             if (optyPoolEnabled[_optyToken] == true) {
-                uint256 _deltaBlocksUser = sub_(optyUserStateInPool[_optyToken][holder].block, optyPoolStartBlock[_optyToken]);
+                uint256 _deltaSecondsUser = sub_(optyUserStateInPool[_optyToken][holder].timestamp, optyPoolStartTimestamp[_optyToken]);
                 uint256 _currentOptyPoolIndex = currentOptyPoolIndex(_optyToken);
                 uint256 _supplierDelta =
                     mul_(
                         IERC20(_optyToken).balanceOf(holder),
                         sub_(
-                            mul_(_currentOptyPoolIndex, sub_(getBlockNumber(), optyPoolStartBlock[_optyToken])),
-                            mul_(optyUserStateInPool[_optyToken][holder].index, _deltaBlocksUser)
+                            mul_(_currentOptyPoolIndex, sub_(getBlockTimestamp(), optyPoolStartTimestamp[_optyToken])),
+                            mul_(optyUserStateInPool[_optyToken][holder].index, _deltaSecondsUser)
                         )
                     );
                 _totalOpty = add_(_totalOpty, _supplierDelta);
@@ -84,15 +97,18 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
     }
 
     function currentOptyPoolIndex(address optyPool) public view returns (uint256) {
-        uint256 _deltaBlocksSinceStart = sub_(getBlockNumber(), optyPoolStartBlock[optyPool]);
-        uint256 _deltaBlocks = sub_(getBlockNumber(), uint256(optyPoolState[optyPool].block));
+        uint256 _deltaSecondsSinceStart = sub_(getBlockTimestamp(), optyPoolStartTimestamp[optyPool]);
+        uint256 _deltaSeconds = sub_(getBlockTimestamp(), uint256(optyPoolState[optyPool].timestamp));
         uint256 _supplyTokens = IERC20(optyPool).totalSupply();
-        uint256 _optyAccrued = mul_(_deltaBlocks, optyPoolRatePerBlock[optyPool]);
+        uint256 _optyAccrued = mul_(_deltaSeconds, optyPoolRatePerSecond[optyPool]);
         uint256 _ratio = _supplyTokens > 0 ? div_(mul_(_optyAccrued, 1e18), _supplyTokens) : uint256(0);
         uint256 _index =
             div_(
-                add_(mul_(optyPoolState[optyPool].index, sub_(uint256(optyPoolState[optyPool].block), optyPoolStartBlock[optyPool])), _ratio),
-                _deltaBlocksSinceStart
+                add_(
+                    mul_(optyPoolState[optyPool].index, sub_(uint256(optyPoolState[optyPool].timestamp), optyPoolStartTimestamp[optyPool])),
+                    _ratio
+                ),
+                _deltaSecondsSinceStart
             );
         return _index;
     }
@@ -102,15 +118,15 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
      * @param supplier The address to calculate contributor rewards for
      */
     function updateSupplierRewards(address optyToken, address supplier) public {
-        if (IERC20(optyToken).balanceOf(supplier) > 0 && lastUserUpdate[optyToken][supplier] != getBlockNumber()) {
-            uint256 _deltaBlocksPool = sub_(getBlockNumber(), optyPoolStartBlock[optyToken]);
-            uint256 _deltaBlocksUser = sub_(optyUserStateInPool[optyToken][supplier].block, optyPoolStartBlock[optyToken]);
+        if (IERC20(optyToken).balanceOf(supplier) > 0 && lastUserUpdate[optyToken][supplier] != getBlockTimestamp()) {
+            uint256 _deltaSecondsPool = sub_(getBlockTimestamp(), optyPoolStartTimestamp[optyToken]);
+            uint256 _deltaSecondsUser = sub_(optyUserStateInPool[optyToken][supplier].timestamp, optyPoolStartTimestamp[optyToken]);
             uint256 _supplierTokens = IERC20(optyToken).balanceOf(supplier);
             uint256 _currentOptyPoolIndex = currentOptyPoolIndex(optyToken);
             uint256 _supplierDelta =
                 mul_(
                     _supplierTokens,
-                    sub_(mul_(_currentOptyPoolIndex, _deltaBlocksPool), mul_(optyUserStateInPool[optyToken][supplier].index, _deltaBlocksUser))
+                    sub_(mul_(_currentOptyPoolIndex, _deltaSecondsPool), mul_(optyUserStateInPool[optyToken][supplier].index, _deltaSecondsUser))
                 );
             uint256 _supplierAccrued = add_(optyAccrued[supplier], _supplierDelta);
             optyAccrued[supplier] = _supplierAccrued;
@@ -127,9 +143,9 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
      * @notice Set the OPTY rate for a specific pool
      * @return The amount of OPTY which was NOT transferred to the user
      */
-    function updateOptyPoolRatePerBlockAndLPToken(address optyPool) public returns (bool) {
-        optyPoolRatePerBlockAndLPToken[optyPool] = IERC20(optyPool).totalSupply() > 0
-            ? div_(mul_(optyPoolRatePerBlock[optyPool], 1e18), IERC20(optyPool).totalSupply())
+    function updateOptyPoolRatePerSecondAndLPToken(address optyPool) public returns (bool) {
+        optyPoolRatePerSecondAndLPToken[optyPool] = IERC20(optyPool).totalSupply() > 0
+            ? div_(mul_(optyPoolRatePerSecond[optyPool], 1e18), IERC20(optyPool).totalSupply())
             : uint256(0);
         return true;
     }
@@ -145,19 +161,22 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
             optyPoolState[optyPool].index = uint224(optyPoolRatePerSecondAndLPToken[optyPool]);
             return optyPoolState[optyPool].index;
         } else {
-            uint256 _deltaBlocks = sub_(getBlockNumber(), uint256(optyPoolState[optyPool].block));
-            if (_deltaBlocks > 0) {
-                uint256 _deltaBlocksSinceStart = sub_(getBlockNumber(), optyPoolStartBlock[optyPool]);
+            uint256 _deltaSeconds = sub_(getBlockTimestamp(), uint256(optyPoolState[optyPool].timestamp));
+            if (_deltaSeconds > 0) {
+                uint256 _deltaSecondsSinceStart = sub_(getBlockTimestamp(), optyPoolStartTimestamp[optyPool]);
                 uint256 _supplyTokens = IERC20(optyPool).totalSupply();
-                uint256 _optyAccrued = mul_(_deltaBlocks, optyPoolRatePerBlock[optyPool]);
+                uint256 _optyAccrued = mul_(_deltaSeconds, optyPoolRatePerSecond[optyPool]);
                 uint256 _ratio = _supplyTokens > 0 ? div_(mul_(_optyAccrued, 1e18), _supplyTokens) : uint256(0);
                 uint256 _index =
                     div_(
                         add_(
-                            mul_(optyPoolState[optyPool].index, sub_(uint256(optyPoolState[optyPool].block), optyPoolStartBlock[optyPool])),
+                            mul_(
+                                optyPoolState[optyPool].index,
+                                sub_(uint256(optyPoolState[optyPool].timestamp), optyPoolStartTimestamp[optyPool])
+                            ),
                             _ratio
                         ),
-                        _deltaBlocksSinceStart
+                        _deltaSecondsSinceStart
                     );
                 optyPoolState[optyPool] = OptyState({
                     index: safe224(_index, "new index exceeds 224 bits"),
@@ -175,7 +194,7 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
      * @param amount The amount of OPTY to (possibly) transfer
      * @return The amount of OPTY which was NOT transferred to the user
      */
-    function mintOpty(address user, uint256 amount) internal returns (uint256) {
+    function mintOpty(address user, uint256 amount) public returns (uint256) {
         OPTY _opty = OPTY(getOptyAddress());
         require(amount > 0 && user != address(0), "Insufficient amount or invalid address");
         _opty.mint(user, amount);
@@ -187,7 +206,7 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
      * @return The amount of OPTY which was NOT transferred to the user
      */
     function setOptyPoolRate(address optyPool, uint256 rate) public onlyOperator returns (bool) {
-        optyPoolRatePerBlock[optyPool] = rate;
+        optyPoolRatePerSecond[optyPool] = rate;
         return true;
     }
 
@@ -203,15 +222,11 @@ contract OPTYMinter is OPTYMinterStorage, ExponentialNoError, Modifiers {
         return true;
     }
 
-    function setOptyPoolStartBlock(address optyPool, uint256 blockNumber) public returns (bool) {
-        optyPoolStartBlock[optyPool] = blockNumber;
-    }
-
     function getOptyAddress() public pure returns (address) {
         return address(0xBbc96A1676922ce6bA4366c0d30f155514E588c3);
     }
 
-    function getBlockNumber() public view returns (uint256) {
-        return block.number;
+    function getBlockTimestamp() public view returns (uint256) {
+        return block.timestamp;
     }
 }
