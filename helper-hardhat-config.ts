814385f6a30c816e0d06db09790e09b849b19bb5
leodinh <leo.atdinh@gmail.com>
2021-05-05 14:54:58 -0400

    added fork mainet

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -10,6 +10,7 @@ export enum eEthereumNetwork {
   main = "main",
   coverage = "coverage",
   hardhat = "hardhat",
+  forkMain = "forkMain",
   tenderlyMain = "tenderlyMain",
 }
 
@@ -21,6 +22,7 @@ export const NETWORKS_RPC_URL: iEthereumParamsPerNetwork<string> = {
   [eEthereumNetwork.kovan]: "",
   [eEthereumNetwork.ropsten]: "",
   [eEthereumNetwork.main]: "",
+  [eEthereumNetwork.forkMain]: "http://54.89.212.214:8545/",
   [eEthereumNetwork.coverage]: "http://localhost:8555",
   [eEthereumNetwork.hardhat]: "http://localhost:8545",
   [eEthereumNetwork.tenderlyMain]: "",
@@ -33,6 +35,7 @@ export const NETWORKS_DEFAULT_GAS: iEthereumParamsPerNetwork<number> = {
   [eEthereumNetwork.main]: 65 * GWEI,
   [eEthereumNetwork.coverage]: 65 * GWEI,
   [eEthereumNetwork.hardhat]: 65 * GWEI,
+  [eEthereumNetwork.forkMain]: 65 * GWEI,
   [eEthereumNetwork.buidlerevm]: 65 * GWEI,
   [eEthereumNetwork.tenderlyMain]: 0.01 * GWEI,
 };
