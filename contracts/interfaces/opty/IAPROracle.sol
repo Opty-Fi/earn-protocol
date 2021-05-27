1114819d87c4a5789ef589b85d356a58417bfc57
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2021-05-27 22:20:39 +0200

    Added functions to IAPROracle.sol interface

--- contracts/interfaces/opty/IAPROracle.sol
+++ contracts/interfaces/opty/IAPROracle.sol
@@ -4,5 +4,11 @@ pragma solidity ^0.6.10;
 pragma experimental ABIEncoderV2;
 
 interface IAPROracle {
+    function getCompoundAPR(address token) external view returns (uint256);
+
+    function getAaveV1APR(address token) external view returns (address, uint256);
+
+    function getAaveV2APR(address token) external view returns (address, uint256);
+
     function getBestAPR(bytes32 tokensHash) external view returns (bytes32);
 }
