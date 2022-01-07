296a0407b33bf2a33ba066ecaad82274676067ca
leodinh <leo.atdinh@gmail.com>
2022-01-07 17:19:03 -0500

    fixed data

--- helpers/data/adapter-with-strategies.ts
+++ helpers/data/adapter-with-strategies.ts
@@ -1,5 +1,5 @@
-import { default as DaiStrategies } from "../../.optyfi-sdk/ethereum/DAI.json";
-import { default as SlpStrategies } from "../../.optyfi-sdk/ethereum/SLP_WETH_USDC.json";
+import { default as DaiStrategies } from "optyfi-sdk/ethereum/strategies/dai.json";
+import { default as SlpStrategies } from "optyfi-sdk/ethereum/strategies/slp.json";
 import { ADAPTER_WITH_STRATEGIES_DATA } from "../type";
 import {
   AAVE_V1_ADAPTER_NAME,
@@ -16,15 +16,15 @@ import {
 } from "../constants/adapters";
 
 export const TypedAdapterStrategies: ADAPTER_WITH_STRATEGIES_DATA = {
-  [CURVE_SWAP_POOL_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-CurveSwapPool-3Crv"]],
-  [CURVE_DEPOSIT_POOL_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-CurveDepositPool-cDAI+cUSDC"]],
-  [COMPOUND_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-Compound-cDAI"]],
-  [AAVE_V1_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-AaveV1-aDAI"]],
-  [AAVE_V2_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-AaveV2-aDAI"]],
-  [DFORCE_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-DForce-dDAI"]],
-  [DYDX_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-DyDx"]],
-  [YVAULT_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-YVault-yDAI"]],
-  [FULCRUM_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-Fulcrum-iDAI"]],
-  [HARVEST_V1_ADAPTER_NAME]: [DaiStrategies["DAI-DEPOSIT-HarvestV1-fDAI"]],
-  [SUSHISWAP_ADAPTER_NAME]: [SlpStrategies["SLP_WETH_USDC-DEPOSIT-Sushiswap"]],
+  [CURVE_SWAP_POOL_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-CurveSwapPool-3Crv"]],
+  [CURVE_DEPOSIT_POOL_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-CurveDepositPool-cDAI+cUSDC"]],
+  [COMPOUND_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-Compound-cDAI"]],
+  [AAVE_V1_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-AaveV1-aDAI"]],
+  [AAVE_V2_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-AaveV2-aDAI"]],
+  [DFORCE_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-DForce-dDAI"]],
+  [DYDX_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-DyDx"]],
+  [YVAULT_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-YVault-yDAI"]],
+  [FULCRUM_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-Fulcrum-iDAI"]],
+  [HARVEST_V1_ADAPTER_NAME]: [DaiStrategies["dai-DEPOSIT-HarvestV1-fDAI"]],
+  [SUSHISWAP_ADAPTER_NAME]: [SlpStrategies["slp-DEPOSIT-Sushiswap"]],
 };
