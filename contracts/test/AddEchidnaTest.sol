48be3997f75f37bb8b704ba6569db1f61821b178
bzrra.rafael@gmail.com <rafael@opty.fi>
2022-05-19 17:37:33 -0400

    feat(echidna): added echidna

---
+++ contracts/test/AddEchidnaTest.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.5;
+
+contract AddContractEchidnaTest {
+    function echidna_test_pass() public pure returns (bool) {
+        return true;
+    }
+}
