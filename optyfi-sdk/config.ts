ebc534b062dc51ec52bb88857e1f3cc403c4b837
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2022-05-30 11:34:58 +0200

    ci(optyfi-sdk): fetch pools from Moralis

--- optyfi-sdk/config.ts
+++ optyfi-sdk/config.ts
@@ -6,22 +6,22 @@ import { legos as PolygonLegos } from "@optyfi/defi-legos/polygon";
 
 export default defineConfig({
   // TODO: ONLY WORKS FOR ONE NETWORK
-  // [NETWORKS_ID.MAINNET]: {
-  //   strategies: {
-  //     dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-  //     usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-  //     slp: "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0",
-  //   },
-  //   protocols: ETH_PROTOCOLS,
-  // },
-  [NETWORKS_ID.POLYGON]: {
+  [NETWORKS_ID.MAINNET]: {
     strategies: {
-      usdc: PolygonLegos.tokens.USDC,
-      wmatic: PolygonLegos.tokens.WMATIC,
-      dai: PolygonLegos.tokens.DAI,
-      slp: PolygonLegos.tokens.SLP,
-      weth: PolygonLegos.tokens.WETH,
+      dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+      usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      slp: "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0",
     },
-    protocols: POLYGON_PROTOCOLS,
+    protocols: ETH_PROTOCOLS,
   },
+  // [NETWORKS_ID.POLYGON]: {
+  //   strategies: {
+  //     usdc: PolygonLegos.tokens.USDC,
+  //     wmatic: PolygonLegos.tokens.WMATIC,
+  //     dai: PolygonLegos.tokens.DAI,
+  //     slp: PolygonLegos.tokens.SLP,
+  //     weth: PolygonLegos.tokens.WETH,
+  //   },
+  //   protocols: POLYGON_PROTOCOLS,
+  // },
 });
