f8214652822f8461346422109ba77813729f1da2
Mario Gutiérrez Valero <m.gutierrez.valero@me.com>
2022-05-30 11:47:39 +0200

    refactor(optyfi-sdk): removed hardcoded addresses

--- optyfi-sdk/config.ts
+++ optyfi-sdk/config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from "./src";
 import { PROTOCOLS as ETH_PROTOCOLS } from "../helpers/constants/adapters";
 import { PROTOCOLS as POLYGON_PROTOCOLS } from "../helpers/constants/adapters-polygon";
 import { NETWORKS_ID } from "../helpers/constants/network";
-import { legos as PolygonLegos } from "@optyfi/defi-legos/polygon";
+// import { legos as EthereumLegos } from "@optyfi/defi-legos/ethereum";
+import tokens from "../helpers/data/plain_tokens.json";
 
 export default defineConfig({
   // TODO: ONLY WORKS FOR ONE NETWORK
   [NETWORKS_ID.MAINNET]: {
     strategies: {
-      dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-      usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
-      slp: "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0",
+      usdc: tokens.USDC,
+      dai: tokens.DAI,
+      slp: tokens.SLP_WETH_USDC,
+      weth: tokens.WETH,
     },
     protocols: ETH_PROTOCOLS,
   },
