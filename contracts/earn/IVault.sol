34211b825b036e7cbe38c16d1ab038b7b97235b3
NouDaimon <peric.iason@gmail.com>
2022-06-21 10:19:28 +0300

    feat(interface): earn vault interface

---
+++ contracts/earn/IVault.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+/**
+ * @title Interface for opty.fi's interest bearing vault
+ * @author opty.fi
+ */
+interface IVault {
+    /**
+     * @notice Deposit underlying tokens to the vault
+     * @dev Mint the shares right away as per oracle based price per full share value
+     * @param _userDepositUT Amount in underlying token
+     * @param _accountsProof merkle proof for caller
+     * @param _codesProof merkle proof for code hash if caller is smart contract
+     */
+    function userDepositVault(
+        uint256 _userDepositUT,
+        bytes32[] calldata _accountsProof,
+        bytes32[] calldata _codesProof
+    ) external;
+
+    /**
+     * @notice redeems the vault shares and transfers underlying token to the withdrawer
+     * @dev Burn the shares right away as per oracle based price per full share value
+     * @param _userWithdrawVT amount in vault token
+     * @param _accountsProof merkle proof for caller
+     * @param _codesProof merkle proof for code hash if caller is smart contract
+     */
+    function userWithdrawVault(
+        uint256 _userWithdrawVT,
+        bytes32[] calldata _accountsProof,
+        bytes32[] calldata _codesProof
+    ) external;
+
+    /**
+     * @notice Calculate the value of a vault share in underlying token
+     *         read-only function to compute price per share of the vault
+     *         Note : This function calculates the pricePerFullShare (i.e. the number of underlyingTokens
+     *         per each vaultToken entitles you to).
+     *
+     *         Please note the following quantities are included in underlyingTokens :
+     *         - underlyingTokens in vault that are not yet deployed in strategy
+     *
+     *        Please note the following quantities are *NOT* included in underlyingTokens :
+     *         - unclaimed reward tokens from the current or past strategies
+     *         - claimed reward tokens that are not yet harvested to underlyingTokens
+     *         - any tokens other than underlyingTokens of the vault.
+     *
+     *         Please note we rely on the getOraValueUT() function of StrategyBuilder which in turn relies on individual
+     *         protocol adapters to obtain the current underlying token amount. Thus we are relying on a third party
+     *         contract (i.e. an oracle). This oracle should be made resilient via best practices.
+     * @return The underlying token worth a vault share is
+     */
+    function getPricePerFullShare() external view returns (uint256);
+}
