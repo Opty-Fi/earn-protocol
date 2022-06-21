12db1b053a6a056207f9c04d79cca987fc73e4e6
NouDaimon <peric.iason@gmail.com>
2022-06-21 11:47:19 +0300

    feat(order): add price feed to order

--- contracts/DataTypes.sol
+++ contracts/DataTypes.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+
 /**
  * @title DataTypes for Limit Orders
  * @author OptyFi
@@ -23,6 +25,7 @@ library DataTypes {
         uint256 endTime;
         address maker;
         address vault;
+        AggregatorV3Interface priceFeed;
         Side side;
     }
 
