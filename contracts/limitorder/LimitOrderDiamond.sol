71c4117cfe26676d45c11f1d31856cf2d1e4e54c
NouDaimon <peric.iason@gmail.com>
2022-07-01 15:12:35 +0300

    feat(limitOrder): add empty diamond

---
+++ contracts/limitorder/LimitOrderDiamond.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import { SolidStateDiamond } from '@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol';
+
+contract LimitOrderDiamond is SolidStateDiamond {
+    constructor() {}
+}
