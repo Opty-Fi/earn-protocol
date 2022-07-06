c8fb9dd9c37b93bc62cb5defc27a79a92a113713
NouDaimon <peric.iason@gmail.com>
2022-07-06 17:43:41 +0300

    fix(ethers): import nomicalabs ethers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
 import '@typechain/hardhat';
 import 'hardhat-docgen';
 import 'hardhat-gas-reporter';
