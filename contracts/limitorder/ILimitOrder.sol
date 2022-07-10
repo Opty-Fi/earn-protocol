471c7a6203cae8f4f964a02657819a52c287ed2a
NouDaimon <peric.iason@gmail.com>
2022-07-10 21:14:46 +0300

    feat(events): added ILimitOrderInternal to ILimitOrder

--- contracts/limitorder/ILimitOrder.sol
+++ contracts/limitorder/ILimitOrder.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.15;
 import { ILimitOrderSettings } from './ILimitOrderSettings.sol';
 import { ILimitOrderActions } from './ILimitOrderActions.sol';
 import { ILimitOrderView } from './ILimitOrderView.sol';
+import { ILimitOrderInternal } from './ILimitOrderInternal.sol';
 
 interface ILimitOrder is
     ILimitOrderActions,
     ILimitOrderSettings,
-    ILimitOrderView
+    ILimitOrderView,
+    ILimitOrderInternal
 {}
