042acf742fa5809aa0665b29da36049b2916da76
NouDaimon <peric.iason@gmail.com>
2022-07-21 13:24:43 +0300

    feat(swap): errors natspec

--- contracts/swap/Errors.sol
+++ contracts/swap/Errors.sol
@@ -6,12 +6,43 @@ pragma solidity ^0.8.15;
  * @author OptyFi
  */
 library Errors {
+    /**
+     * @notice call to TokenTransferProxy contract
+     */
     error TokenTransferProxyCall();
+
+    /**
+     * @notice calling TransferFrom
+     */
     error TransferFromCall();
+
+    /**
+     * @notice external call failed
+     */
     error ExternalCallFailure();
+
+    /**
+     * @notice returned tokens too few
+     */
     error InsufficientReturn();
+
+    /**
+     * @notice swapDeadline expired
+     */
     error DeadlineBreach();
+
+    /**
+     * @notice incorrect ETH amount sent
+     */
     error ETHValueMismatch();
+
+    /**
+     * @notice expected tokens returned are 0
+     */
     error ZeroExpectedReturns();
+
+    /**
+     * @notice arrays in SwapData.exchangeData have wrong lengths
+     */
     error ExchangeDataArrayMismatch();
 }
