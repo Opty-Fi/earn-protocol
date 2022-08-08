e41ef8179d64f0f3134d0770c1f01899ebb5c9a0
bzrra.rafael@gmail.com <rafael@opty.fi>
2022-08-08 18:44:34 -0400

    feat(zapper): added zapper contracts

---
+++ contracts/protocol/zap/IOptyFiZapper.sol
@@ -0,0 +1,28 @@
+//SPDX-license-identifier: MIT
+pragma solidity ^0.8.15;
+
+import { DataTypes } from "./DataTypes.sol";
+
+/**
+ * @title OptyFiZapper interface
+ * @author OptyFi
+ */
+interface IOptyFiZapper {
+    function zapIn(
+        address _token,
+        uint256 _amount,
+        bytes memory _permitParams,
+        DataTypes.ZapData memory _zapParams
+    ) external payable returns (uint256 sharesReceived);
+
+    function zapOut(
+        address _token,
+        uint256 _amount,
+        bytes memory _permitParams,
+        DataTypes.ZapData memory _zapParams
+    ) external returns (uint256 receivedAmount);
+
+    function setSwapper(address _swapper) external;
+
+    function getSwapper() external view returns (address);
+}
