9598798581841b340122e59b2e14d924c52b4adf
bzrra.rafael@gmail.com <rafael@opty.fi>
2022-08-18 18:23:14 -0400

    feat(zap): added datatypes

---
+++ contracts/protocol/zap/DataTypes.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/**
+ * @title Library for custom structs for OptyFiZapper
+ * @author OptyFi
+ */
+library DataTypes {
+    /**
+     * @param vault address of the vault
+     * @param toAmount amount of toToken to receive
+     * @param callees array of addresses to call (DEX addresses)
+     * @param exchangeData calldata to execute on callees
+     * @param startIndexes the index of the beginning of each call in exchangeData
+     * @param values array of encoded values for each call in exchangeData
+     * @param permit ERC2612 permit
+     * @param deadline timestamp until which swap may be fulfilled
+     * @param accountsProof merkle proof for caller
+     * @param codesProof merkle proof for code hash if caller is smart contract
+     */
+    struct ZapData {
+        address vault;
+        uint256 toAmount;
+        uint256 deadline;
+        bytes exchangeData;
+        bytes permit;
+        address[] callees;
+        uint256[] startIndexes;
+        uint256[] values;
+        bytes32[] accountsProof;
+        bytes32[] codesProof;
+    }
+}
