9f168196d62a48e1f06d3d6be99f1455a7908c9e
bzrra.rafael@gmail.com <rafael@opty.fi>
2022-08-19 15:31:02 -0400

    fix(eip712): added explanation to eip712 change

--- contracts/utils/EIP712.sol
+++ contracts/utils/EIP712.sol
@@ -3,23 +3,8 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 /**
- * @dev https://eips.ethereum.org/EIPS/eip-712[EIP 712] is a standard for hashing and signing of typed structured data.
- *
- * The encoding specified in the EIP is very generic, and such a generic implementation in Solidity is not feasible,
- * thus this contract does not implement the encoding itself. Protocols need to implement the type-specific encoding
- * they need in their contracts using a combination of `abi.encode` and `keccak256`.
- *
- * This contract implements the EIP 712 domain separator ({_domainSeparatorV4}) that is used as part of the encoding
- * scheme, and the final step of the encoding to obtain the message digest that is then signed via ECDSA
- * ({_hashTypedDataV4}).
- *
- * The implementation of the domain separator was designed to be as efficient as possible while still properly updating
- * the chain id to protect against replay attacks on an eventual fork of the chain.
- *
- * NOTE: This contract implements the version of the encoding known as "v4", as implemented by the JSON RPC method
- * https://docs.metamask.io/guide/signing-data.html[`eth_signTypedDataV4` in MetaMask].
- *
- * _Available since v3.4._
+ * @dev This contract is a modified version of the OpenZeppelin v3.4 implementation as it does not cach
+ * the domain seprator and chain_id in order to work with OptyFi's Vault Upgradeability pattern
  */
 abstract contract EIP712 {
     /* solhint-disable var-name-mixedcase */
@@ -86,7 +71,8 @@ abstract contract EIP712 {
     }
 
     function _getChainId() private view returns (uint256 chainId) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        this; // silence state mutability warning without generating bytecode
+        // - see https://github.com/ethereum/solidity/issues/2691
         // solhint-disable-next-line no-inline-assembly
         assembly {
             chainId := chainid()
