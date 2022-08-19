3e1a4498bafb84d029146f7c34d43f95e93ca3a6
bzrra.rafael@gmail.com <rafael@opty.fi>
2022-08-19 16:35:45 -0400

    fix(zapper): import iweth, add errors lib, add comments

--- contracts/protocol/zap/OptyFiZapper.sol
+++ contracts/protocol/zap/OptyFiZapper.sol
@@ -5,18 +5,24 @@ import "@openzeppelin/contracts-0.8.x/token/ERC20/extensions/draft-IERC20Permit.
 import "@openzeppelin/contracts-0.8.x/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-0.8.x/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts-0.8.x/access/Ownable.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IWETH.sol";
+import { Errors } from "./Errors.sol";
 import { IOptyFiZapper } from "./IOptyFiZapper.sol";
 import { ISwapper } from "../optyfi-swapper/contracts/swap/ISwapper.sol";
 import { DataTypes as SwapTypes } from "../optyfi-swapper/contracts/swap/DataTypes.sol";
 import { DataTypes } from "./DataTypes.sol";
 
-interface IWETH is IERC20 {
-    function deposit() external payable;
-
-    function withdraw(uint256 wad) external;
-}
-
-interface IVault is IERC20 {
+interface IVault {
+    /**
+     * @notice Deposit underlying tokens to the vault
+     * @dev Mint the shares right away as per oracle based price per full share value
+     * @param _beneficiary the address of the deposit beneficiary,
+     *        if _beneficiary = address(0) => _beneficiary = msg.sender
+     * @param _userDepositUT Amount in underlying token
+     * @param _permitParams permit parameters: amount, deadline, v, s, r
+     * @param _accountsProof merkle proof for caller
+     * @param _codesProof merkle proof for code hash if caller is smart contract
+     */
     function userDepositVault(
         address _beneficiary,
         uint256 _userDepositUT,
@@ -25,6 +31,14 @@ interface IVault is IERC20 {
         bytes32[] calldata _codesProof
     ) external returns (uint256);
 
+    /**
+     * @notice redeems the vault shares and transfers underlying token to `_beneficiary`
+     * @dev Burn the shares right away as per oracle based price per full share value
+     * @param _receiver the address which will receive the underlying tokens
+     * @param _userWithdrawVT amount in vault token
+     * @param _accountsProof merkle proof for caller
+     * @param _codesProof merkle proof for code hash if caller is smart contract
+     */
     function userWithdrawVault(
         address _receiver,
         uint256 _userWithdrawVT,
@@ -32,6 +46,9 @@ interface IVault is IERC20 {
         bytes32[] calldata _codesProof
     ) external returns (uint256);
 
+    /**
+     * @dev the vault underlying token contract address
+     */
     function underlyingToken() external returns (address);
 }
 
@@ -57,9 +74,11 @@ contract OptyFiZapper is IOptyFiZapper, Ownable {
         uint256 _amount,
         bytes memory _permitParams,
         DataTypes.ZapData memory _zapParams
-    ) external payable override returns (uint256) {
-        address underlyingToken = _getUnderlyingToken(_zapParams.vault);
-        require(underlyingToken != _token, "Invalid Token");
+    ) external payable override returns (uint256 sharesReceived) {
+        address vaultUnderlyingToken = _vaultUnderlyingToken(_zapParams.vault);
+        if (vaultUnderlyingToken == _token) {
+            revert Errors.InvalidToken();
+        }
 
         if (_token != ETH) {
             _permit(_token, _permitParams);
@@ -67,38 +86,34 @@ contract OptyFiZapper is IOptyFiZapper, Ownable {
             _approveTokenIfNeeded(_token, swapper.tokenTransferProxy(), _amount);
         }
 
-        SwapTypes.SwapData memory swapData =
-            SwapTypes.SwapData(
-                _token,
-                underlyingToken,
-                _amount,
-                _zapParams.toAmount,
-                _zapParams.callees,
-                _zapParams.exchangeData,
-                _zapParams.startIndexes,
-                _zapParams.values,
-                payable(address(this)),
-                _zapParams.permit,
-                _zapParams.deadline
-            );
+        SwapTypes.SwapData memory swapData = SwapTypes.SwapData(
+            _token,
+            vaultUnderlyingToken,
+            _amount,
+            _zapParams.toAmount,
+            _zapParams.callees,
+            _zapParams.exchangeData,
+            _zapParams.startIndexes,
+            _zapParams.values,
+            payable(address(this)),
+            _zapParams.permit,
+            _zapParams.deadline
+        );
 
-        (uint256 receivedAmount, uint256 returnedBalance) = swapper.swap{ value: msg.value }(swapData);
-        if (returnedBalance > 0) {
-            IERC20(_token).safeTransfer(msg.sender, returnedBalance);
+        (uint256 receivedAmount, uint256 unused) = swapper.swap{ value: msg.value }(swapData);
+        if (unused > 0) {
+            IERC20(_token).safeTransfer(msg.sender, unused);
         }
 
-        _approveTokenIfNeeded(underlyingToken, _zapParams.vault, receivedAmount);
+        _approveTokenIfNeeded(vaultUnderlyingToken, _zapParams.vault, receivedAmount);
 
-        uint256 sharesReceived =
-            IVault(_zapParams.vault).userDepositVault(
-                msg.sender,
-                receivedAmount,
-                _zapParams.permit,
-                _zapParams.accountsProof,
-                _zapParams.codesProof
-            );
-
-        return sharesReceived;
+        sharesReceived = IVault(_zapParams.vault).userDepositVault(
+            msg.sender,
+            receivedAmount,
+            _zapParams.permit,
+            _zapParams.accountsProof,
+            _zapParams.codesProof
+        );
     }
 
     /**
@@ -109,44 +124,43 @@ contract OptyFiZapper is IOptyFiZapper, Ownable {
         uint256 _amount,
         bytes memory _permitParams,
         DataTypes.ZapData memory _zapParams
-    ) external override returns (uint256) {
-        address underlyingToken = _getUnderlyingToken(_zapParams.vault);
-        require(underlyingToken != _token, "Invalid Token");
+    ) external override returns (uint256 receivedAmount) {
+        address vaultUnderlyingToken = _vaultUnderlyingToken(_zapParams.vault);
+        if (vaultUnderlyingToken == _token) {
+            revert Errors.InvalidToken();
+        }
 
         _permit(_zapParams.vault, _permitParams);
 
         IERC20(_zapParams.vault).safeTransferFrom(msg.sender, address(this), _amount);
-        uint256 amountToSwap =
-            IVault(_zapParams.vault).userWithdrawVault(
-                address(this),
-                _amount,
-                _zapParams.accountsProof,
-                _zapParams.codesProof
-            );
+        uint256 swapAmount = IVault(_zapParams.vault).userWithdrawVault(
+            address(this),
+            _amount,
+            _zapParams.accountsProof,
+            _zapParams.codesProof
+        );
 
-        _approveTokenIfNeeded(underlyingToken, swapper.tokenTransferProxy(), amountToSwap);
+        _approveTokenIfNeeded(vaultUnderlyingToken, swapper.tokenTransferProxy(), swapAmount);
 
-        SwapTypes.SwapData memory swapData =
-            SwapTypes.SwapData(
-                underlyingToken,
-                _token,
-                amountToSwap,
-                _zapParams.toAmount,
-                _zapParams.callees,
-                _zapParams.exchangeData,
-                _zapParams.startIndexes,
-                _zapParams.values,
-                payable(msg.sender),
-                _zapParams.permit,
-                _zapParams.deadline
-            );
+        SwapTypes.SwapData memory swapData = SwapTypes.SwapData(
+            vaultUnderlyingToken,
+            _token,
+            swapAmount,
+            _zapParams.toAmount,
+            _zapParams.callees,
+            _zapParams.exchangeData,
+            _zapParams.startIndexes,
+            _zapParams.values,
+            payable(msg.sender),
+            _zapParams.permit,
+            _zapParams.deadline
+        );
 
-        (uint256 receivedAmount, uint256 returnedBalance) = swapper.swap(swapData);
-        if (returnedBalance > 0) {
-            IERC20(_token).safeTransfer(msg.sender, returnedBalance);
+        uint256 unused;
+        (receivedAmount, unused) = swapper.swap(swapData);
+        if (unused > 0) {
+            IERC20(_token).safeTransfer(msg.sender, unused);
         }
-
-        return receivedAmount;
     }
 
     /**
@@ -166,7 +180,7 @@ contract OptyFiZapper is IOptyFiZapper, Ownable {
     /**
      * @dev function to get the underlying token of the OptyFi Vault
      */
-    function _getUnderlyingToken(address _vault) internal returns (address) {
+    function _vaultUnderlyingToken(address _vault) internal returns (address) {
         return IVault(_vault).underlyingToken();
     }
 
@@ -178,7 +192,9 @@ contract OptyFiZapper is IOptyFiZapper, Ownable {
     function _permit(address _token, bytes memory _permitParams) internal {
         if (_permitParams.length == 32 * 7) {
             (bool success, ) = _token.call(abi.encodePacked(IERC20Permit.permit.selector, _permitParams));
-            require(success, "Permit failed");
+            if (!success) {
+                revert Errors.PermitFailed();
+            }
         }
     }
 
