b2cc1c903b1ed16ab1d419f06e560d13014913a8
albuquerque-rafael <rafael@opty.fi>
2022-09-12 18:18:07 -0400

    fix(test): remove condition

--- test/test-opty/migration/002_polygon_upgrade/usdcRebalance.ts
+++ test/test-opty/migration/002_polygon_upgrade/usdcRebalance.ts
@@ -1,16 +1,15 @@
 import { getAddress } from "ethers/lib/utils";
 import { ethers } from "hardhat";
+import { ESSENTIAL_CONTRACTS } from "../../../../helpers/constants/essential-contracts-name";
 import { oldAbis } from "../../../../helpers/data/oldAbis";
 import { opUSDCgrow } from "../../_deployments/polygon.json";
 
 export async function usdcRebalance(): Promise<void> {
-  if (getAddress(opUSDCgrow.Vault) == getAddress(opUSDCgrow.Vault)) {
-    const opUSDCgrowInstance = await ethers.getContractAt(oldAbis.oldVault, opUSDCgrow.VaultProxy);
-    const usdcCurrentStrategyHash = await opUSDCgrowInstance.investStrategyHash();
+  const opUSDCgrowInstance = await ethers.getContractAt(oldAbis.oldVault, opUSDCgrow.VaultProxy);
+  const usdcCurrentStrategyHash = await opUSDCgrowInstance.investStrategyHash();
 
-    if (usdcCurrentStrategyHash != ethers.constants.HashZero) {
-      const tx = await opUSDCgrowInstance.rebalance();
-      await tx.wait(1);
-    }
+  if (usdcCurrentStrategyHash != ethers.constants.HashZero) {
+    const tx = await opUSDCgrowInstance.rebalance();
+    await tx.wait(1);
   }
 }
