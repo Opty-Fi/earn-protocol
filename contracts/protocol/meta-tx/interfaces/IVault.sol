48ef024db59338ef32501b9f4ec84b88766dd262
albuquerque-rafael <rafael@opty.fi>
2022-09-27 21:53:02 -0400

    fix(interface): fix ivault

--- contracts/protocol/meta-tx/interfaces/IVault.sol
+++ contracts/protocol/meta-tx/interfaces/IVault.sol
@@ -8,12 +8,14 @@ interface IVault {
      * @param _beneficiary the address of the deposit beneficiary,
      *        if _beneficiary = address(0) => _beneficiary = msg.sender
      * @param _userDepositUT Amount in underlying token
+     * @param _expectedOutput expected output
      * @param _permitParams permit parameters: amount, deadline, v, s, r
      * @param _accountsProof merkle proof for caller
      */
     function userDepositVault(
         address _beneficiary,
         uint256 _userDepositUT,
+        uint256 _expectedOutput,
         bytes calldata _permitParams,
         bytes32[] calldata _accountsProof
     ) external returns (uint256);
@@ -23,11 +25,13 @@ interface IVault {
      * @dev Burn the shares right away as per oracle based price per full share value
      * @param _receiver the address which will receive the underlying tokens
      * @param _userWithdrawVT amount in vault token
+     * @param _expectedOutput expected output
      * @param _accountsProof merkle proof for caller
      */
     function userWithdrawVault(
         address _receiver,
         uint256 _userWithdrawVT,
+        uint256 _expectedOutput,
         bytes32[] calldata _accountsProof
     ) external returns (uint256);
 
