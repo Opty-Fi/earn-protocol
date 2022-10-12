42ce936a639e1aee4dde903a43ad596f5bc03744
albuquerque-rafael <rafael@opty.fi>
2022-10-12 06:39:18 -0400

    feat(gsn): gsn contracts

---
+++ contracts/mocks/contracts/TestToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    /* solhint-disable no-empty-blocks */
+    constructor(string memory name, string memory symbol) public ERC20(name, symbol) {}
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
