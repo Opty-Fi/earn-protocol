bb219cdf60b1ec858e4ce2a5255d096a7adfaa86
dhruvinparikh <imsilent93@gmail.com>
2022-11-23 22:16:59 -0500

    feat(web3-action): wrap switch strategy

--- tenderly-actions/example.ts
+++
@@ -1,6 +0,0 @@
-import { ActionFn, Context, Event, BlockEvent } from "@tenderly/actions";
-
-export const blockHelloWorldFn: ActionFn = async (context: Context, event: Event) => {
-  const blockEvent = event as BlockEvent;
-  console.log("Block number is: ", blockEvent.blockNumber);
-};
